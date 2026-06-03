4ebba7a78a57ed34c7e4514b316f029b6606e5b0
Gurkirat <gurkiratgamedev@gmail.com>
2026-06-03 12:48:33 +0530

    Added verbose flag

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 from dotenv import load_dotenv
 from google import genai
 import sys
+from google.genai import types
+
 
 def main():
     load_dotenv()
@@ -9,13 +11,20 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    verbose_flag = False
+
     if len(sys.argv) < 2:
         print("we need a prompt")
         sys.exit(1)
 
+    if len(sys.argv) == 3 and sys.argv[1] == "--verbose":
+        verbose_flag = True
+
     prompt = sys.argv[1]
 
-    config = genai.types.GenerateContentConfig(
+    messages: list[types.Content] = [types.Content(role="user", parts=[types.Part(text="prompt")])]
+
+    config = types.GenerateContentConfig(
         max_output_tokens=200,  # Hard limit on response length
         temperature=0.1,  # Low temperature makes the model more direct and concise
         system_instruction="You are a minimalist assistant. Answer in 1-2 short sentences max."
@@ -30,8 +39,9 @@ def main():
     if response is None or response.usage_metadata is None:
         return
 
-    # print(f"prompt token: {response.usage_metadata.prompt_token_count}")
-    # print(f"response token: {response.usage_metadata.candidates_token_count}")
+    if verbose_flag is True:
+        print(f"prompt token: {response.usage_metadata.prompt_token_count}")
+        print(f"response token: {response.usage_metadata.candidates_token_count}")
 
 
 main()
